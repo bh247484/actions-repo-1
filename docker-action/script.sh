92d66c2a9f97394d49be79ffbb94ae5ce748e97d
Brendan Huffman <bh247484@gmail.com>
2021-05-20 17:37:53 -0400

    Attempt branch creation and push

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -5,3 +5,9 @@ git clone https://github.com/bh247484/actions-repo-2
 cd actions-repo-2
 yarn upgrade playbook-ui@9.15.0
 cat package.json
+
+git checkout -b pb-9.15.0-upgrade
+git add .
+git commit -m "Upversion to latest"
+
+git push origin -u pb-9.15.0-upgrade
