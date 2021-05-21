05763cdc42fce12e34a2bed5e81e281e28c359a3
Brendan Huffman <bh247484@gmail.com>
2021-05-21 09:17:19 -0400

    remove flag

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -20,7 +20,7 @@ echo "$SSH_KEY" > /root/.ssh/id_rsa
 # Username for 'https://github.com/bh247484/actions-repo-2': bh247484
 # Password for 'https://USERNAME@some_git_server.com': <PASSWORD>
 
-git clone git@github.com:bh247484/actions-repo-2.git -y
+git clone git@github.com:bh247484/actions-repo-2.git
 cd actions-repo-2
 yarn upgrade playbook-ui@9.15.0
 cat package.json
