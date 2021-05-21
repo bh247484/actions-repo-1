30893061f5cc5ba6e3978afa43901aefb6594077
Brendan Huffman <bh247484@gmail.com>
2021-05-21 09:24:40 -0400

    Revert to gh acc token

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -4,12 +4,12 @@ echo ahoy from script.sh
 
 mkdir /root/.ssh/
 
-echo "$SSH_KEY" > /root/.ssh/id_rsa
+# echo "$SSH_KEY" > /root/.ssh/id_rsa
 # ssh -o "StrictHostKeyChecking=no"
 
-# git config --global credential.helper store
+git config --global credential.helper store
 
-# git config --global url.'https://bh247484:'"$ACC_KEY"':@github.com/'.insteadOf 'https://github.com/'
+git config --global url.'https://bh247484:'"$ACC_KEY"':@github.com/'.insteadOf 'https://github.com/'
 
 git config --global user.email "bh247484@gmail.com"
 # git config --global user.name "$ACC_KEY"
@@ -20,7 +20,7 @@ git config --global user.email "bh247484@gmail.com"
 # Username for 'https://github.com/bh247484/actions-repo-2': bh247484
 # Password for 'https://USERNAME@some_git_server.com': <PASSWORD>
 
-git clone git@github.com:bh247484/actions-repo-2.git
+git clone https://github.com/bh247484/actions-repo-2.git
 cd actions-repo-2
 yarn upgrade playbook-ui@9.15.0
 cat package.json
