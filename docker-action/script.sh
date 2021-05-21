8bf8582b2f8b767b1a98d694c4c49d7c8738a1ee
Brendan Huffman <bh247484@gmail.com>
2021-05-21 11:37:58 -0400

    Cleanup comments

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -6,29 +6,12 @@ set -u
 
 echo ahoy from script.sh
 
-mkdir /root/.ssh/
-
-# echo "$SSH_KEY" > /root/.ssh/id_rsa
-# ssh -o "StrictHostKeyChecking=no"
-
 git config --global credential.helper cache
 # git config --global url.'https://bh247484:'"$ACC_KEY"'@github.com/'.insteadOf 'https://github.com/'
 
 git config --global user.email 'bh247484@gmail.com'
 git config --global user.name 'bh247484'
 
-
-# git clone https://github.com/bh247484/actions-repo-2
-
-# Username for 'https://github.com/bh247484/actions-repo-2': bh247484
-# Password for 'https://USERNAME@some_git_server.com': <PASSWORD>
-
-
-# git config --global credential.helper cache
-# git config --global user.name "bh247484"
-# git config --global user.email "bh247484@gmail.com"
-# git config --global github.user "bh247484"
-# git config --global github.token "$ACC_KEY"
 
 # git clone https://github.com/bh247484/actions-repo-2.git
 git clone https://bh247484:${ACC_KEY}@github.com/bh247484/actions-repo-2.git
@@ -38,6 +21,7 @@ git clone https://bh247484:${ACC_KEY}@github.com/bh247484/actions-repo-2.git
 
 cd actions-repo-2
 git remote set-url origin https://bh247484:${ACC_KEY}@github.com/bh247484/actions-repo-2.git
+
 yarn upgrade playbook-ui@9.15.0
 cat package.json
 
@@ -45,21 +29,4 @@ git checkout -b pb-9.15.0-upgrade
 git add .
 git commit -m "Upversion to latest"
 
-# git remote add origin https://bh247484:"$ACC_KEY"@github.com/bh247484/actions-repo-2.git
-
-# git remote set-url origin https://bh247484:"$ACC_KEY"@github.com/bh247484/actions-repo-2.git
-# git config --global credential.helper store
-
 git push origin -u pb-9.15.0-upgrade
-# git push https://bh247484:"$ACC_KEY"@github.com/bh247484/actions-repo-2.git -u pb-9.15.0-upgrade
-
-# Username for 'https://github.com': 'bh247484'
-# Password for 'https://bh247484@github.com': "$ACC_KEY"
-
-# git push
-
-# expect "Username for 'https://github.com':"
-# send -- "bh247484"
-
-# expect "Password for 'https://bh247484@github.com':"
-# send -- "$ACC_KEY"
