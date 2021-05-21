a1bef81c822d6f5e478c7ca9f92119d80e838abc
Brendan Huffman <bh247484@gmail.com>
2021-05-21 17:13:21 -0400

    auth and better flags

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -43,7 +43,9 @@ git push origin -u pb-9.15.0-upgrade
 # GITHUB_HOST="https://bh247484:$ACC_KEY@github.com/"
 GITHUB_TOKEN="$ACC_KEY"
 
-gh pr create --title "Upversion Playbook to 9.15.0" --body "PR description" --head actions-repo-2:pb-9.15.0-upgrade
+gh auth login --with-token < "$ACC_KEY"
+gh pr create -a bh247484 --base main --title "Upversion Playbook to 9.15.0" --body "PR description" -l enhancement
+# --head actions-repo-2:pb-9.15.0-upgrade
 
 # HUB_VERBOSE=1 hub pull-request
 
