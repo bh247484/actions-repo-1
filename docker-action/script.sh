cfb810216f4561c3f9b1f223c41578698b5a20ae
Brendan Huffman <bh247484@gmail.com>
2021-05-21 10:03:56 -0400

    try single quotes

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -9,8 +9,8 @@ mkdir /root/.ssh/
 
 # git config --global url.'https://bh247484:'"$ACC_KEY"':@github.com/'.insteadOf 'https://github.com/'
 
-git config --global user.email "bh247484@gmail.com"
-git config --global user.name "bh247484"
+git config --global user.email 'bh247484@gmail.com'
+git config --global user.name 'bh247484'
 
 
 # git clone https://github.com/bh247484/actions-repo-2
@@ -44,7 +44,7 @@ git commit -m "Upversion to latest"
 git config --global credential.helper store
 git push origin -u pb-9.15.0-upgrade
 
-Username for 'https://github.com': "bh247484"
+Username for 'https://github.com': 'bh247484'
 Password for 'https://bh247484@github.com': "$ACC_KEY"
 
 git push
