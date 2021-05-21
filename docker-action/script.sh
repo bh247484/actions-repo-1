975578a9447d420331e7d0949b931d35b5e0bd98
Brendan Huffman <bh247484@gmail.com>
2021-05-21 09:39:40 -0400

    try w/ git config

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -7,11 +7,11 @@ mkdir /root/.ssh/
 # echo "$SSH_KEY" > /root/.ssh/id_rsa
 # ssh -o "StrictHostKeyChecking=no"
 
-git config --global credential.helper store
+# git config --global credential.helper cache
 
-git config --global url.'https://bh247484:'"$ACC_KEY"':@github.com/'.insteadOf 'https://github.com/'
+# git config --global url.'https://bh247484:'"$ACC_KEY"':@github.com/'.insteadOf 'https://github.com/'
 
-git config --global user.email "bh247484@gmail.com"
+# git config --global user.email "bh247484@gmail.com"
 # git config --global user.name "$ACC_KEY"
 
 
@@ -20,7 +20,16 @@ git config --global user.email "bh247484@gmail.com"
 # Username for 'https://github.com/bh247484/actions-repo-2': bh247484
 # Password for 'https://USERNAME@some_git_server.com': <PASSWORD>
 
+git config --global user.name "bh247484"
+git config --global user.email "bh247484@gmail.com"
+git config --global github.user "bh247484"
+git config --global github.token "$ACC_KEY"
+
 git clone https://github.com/bh247484/actions-repo-2.git
+
+# Username for 'https://github.com/bh247484/actions-repo-2.git': bh247484
+# Password for 'https://USERNAME@some_git_server.com': <PASSWORD>
+
 cd actions-repo-2
 yarn upgrade playbook-ui@9.15.0
 cat package.json
@@ -32,8 +41,8 @@ git commit -m "Upversion to latest"
 # git remote add origin https://bh247484:"$ACC_KEY"@github.com/bh247484/actions-repo-2.git
 
 git push origin -u pb-9.15.0-upgrade
-expect "Username for 'https://github.com':"
-send -- "bh247484"
+# expect "Username for 'https://github.com':"
+# send -- "bh247484"
 
-expect "Password for 'https://bh247484@github.com':"
-send -- "$ACC_KEY"
+# expect "Password for 'https://bh247484@github.com':"
+# send -- "$ACC_KEY"
