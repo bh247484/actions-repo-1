e859a1fdfea501c0318e77be80e83c64f560d08e
Brendan Huffman <bh247484@gmail.com>
2021-05-20 21:34:20 -0400

    Debug attempt

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -2,13 +2,14 @@
 
 echo ahoy from script.sh
 echo "arg 1: $TEST_SECRET"
-git clone https://github.com/bh247484/actions-repo-2
-cd actions-repo-2
-yarn upgrade playbook-ui@9.15.0
-cat package.json
 
-git checkout -b pb-9.15.0-upgrade
-git add .
-git commit -m "Upversion to latest"
+# git clone https://github.com/bh247484/actions-repo-2
+# cd actions-repo-2
+# yarn upgrade playbook-ui@9.15.0
+# cat package.json
+
+# git checkout -b pb-9.15.0-upgrade
+# git add .
+# git commit -m "Upversion to latest"
 
 # git push origin -u pb-9.15.0-upgrade
