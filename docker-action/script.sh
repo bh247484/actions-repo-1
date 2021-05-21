073e3536670ee0670ed4208f39241dd7bd5e0a78
Brendan Huffman <bh247484@gmail.com>
2021-05-20 22:26:19 -0400

    Another attempt

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -l
 
 echo ahoy from script.sh
+echo with quotes "$ACC_KEY"
+echo with curlies ${ACC_KEY}
 
-git config --global url.\”https://${ACC_KEY}:@github.com/\".insteadOf \“https://github.com/\"
+git config --global url.'https://"$ACC_KEY":@github.com/'.insteadOf 'https://github.com/'
 
 git config --global user.email "bh247484@gmail.com"
 git config --global user.name "bh247484"
