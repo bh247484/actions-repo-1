b0f0c4122bc221d174abfe73e918395c8e13aaa2
Brendan Huffman <bh247484@gmail.com>
2021-05-20 22:56:23 -0400

    Repair with acc token

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -24,8 +24,6 @@ git checkout -b pb-9.15.0-upgrade
 git add .
 git commit -m "Upversion to latest"
 
-git remote -v 
-git remote remove origin 
-git remote add origin git@github.com:bh247484/actions-repo-2.git
+git remote add origin https://bh247484:"$ACC_KEY"@github.com/bh247484/actions-repo-2.git
 
 git push origin -u pb-9.15.0-upgrade
