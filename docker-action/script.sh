4b3c644924925911895c2d0730d3b8feff3cb6e9
Brendan Huffman <bh247484@gmail.com>
2021-05-20 21:48:39 -0400

    gh login attempt

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -1,15 +1,18 @@
 #!/bin/sh -l
 
 echo ahoy from script.sh
-echo "arg 1: $TEST_SECRET"
 
-# git clone https://github.com/bh247484/actions-repo-2
-# cd actions-repo-2
-# yarn upgrade playbook-ui@9.15.0
-# cat package.json
+git config --global url.”https://${ACC_KEY}:@github.com/".insteadOf “https://github.com/"
 
-# git checkout -b pb-9.15.0-upgrade
-# git add .
-# git commit -m "Upversion to latest"
+git config --global user.name "bh247484"
 
-# git push origin -u pb-9.15.0-upgrade
+git clone https://github.com/bh247484/actions-repo-2
+cd actions-repo-2
+yarn upgrade playbook-ui@9.15.0
+cat package.json
+
+git checkout -b pb-9.15.0-upgrade
+git add .
+git commit -m "Upversion to latest"
+
+git push origin -u pb-9.15.0-upgrade
