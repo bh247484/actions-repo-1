f39b3592adf7509c63af0b412c82a865e6c51450
Brendan Huffman <bh247484@gmail.com>
2021-05-20 22:44:59 -0400

    try storing creds

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -1,13 +1,19 @@
 #!/bin/sh -l
 
 echo ahoy from script.sh
-echo with quotes "$ACC_KEY"
-echo with curlies ${ACC_KEY}
+
+git config --global credential.helper store
 
 git config --global url.'https://bh247484:'"$ACC_KEY"':@github.com/'.insteadOf 'https://github.com/'
 
-git config --global user.email "bh247484@gmail.com"
-git config --global user.name "bh247484"
+# git config --global user.email "bh247484@gmail.com"
+# git config --global user.name "$ACC_KEY"
+
+
+# git clone https://github.com/bh247484/actions-repo-2
+
+# Username for 'https://github.com/bh247484/actions-repo-2': bh247484
+# Password for 'https://USERNAME@some_git_server.com': <PASSWORD>
 
 git clone https://github.com/bh247484/actions-repo-2
 cd actions-repo-2
