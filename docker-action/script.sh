81850a06adbc7517cc803e1d46593254183ee323
Brendan Huffman <bh247484@gmail.com>
2021-05-21 09:59:31 -0400

    reintro email, username

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -9,8 +9,8 @@ mkdir /root/.ssh/
 
 # git config --global url.'https://bh247484:'"$ACC_KEY"':@github.com/'.insteadOf 'https://github.com/'
 
-# git config --global user.email "bh247484@gmail.com"
-# git config --global user.name "$ACC_KEY"
+git config --global user.email "bh247484@gmail.com"
+git config --global user.name "bh247484"
 
 
 # git clone https://github.com/bh247484/actions-repo-2
