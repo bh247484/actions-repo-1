2cf2d6ee777c612b1b4c8f92e59e3834d71e167f
Brendan Huffman <bh247484@gmail.com>
2021-05-21 09:55:57 -0400

    gh login

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -7,9 +7,7 @@ mkdir /root/.ssh/
 # echo "$SSH_KEY" > /root/.ssh/id_rsa
 # ssh -o "StrictHostKeyChecking=no"
 
-# git config --global credential.helper cache
-
-git config --global url.'https://bh247484:'"$ACC_KEY"':@github.com/'.insteadOf 'https://github.com/'
+# git config --global url.'https://bh247484:'"$ACC_KEY"':@github.com/'.insteadOf 'https://github.com/'
 
 # git config --global user.email "bh247484@gmail.com"
 # git config --global user.name "$ACC_KEY"
@@ -21,11 +19,11 @@ git config --global url.'https://bh247484:'"$ACC_KEY"':@github.com/'.insteadOf '
 # Password for 'https://USERNAME@some_git_server.com': <PASSWORD>
 
 
-git config --global credential.helper cache
-git config --global user.name "bh247484"
-git config --global user.email "bh247484@gmail.com"
-git config --global github.user "bh247484"
-git config --global github.token "$ACC_KEY"
+# git config --global credential.helper cache
+# git config --global user.name "bh247484"
+# git config --global user.email "bh247484@gmail.com"
+# git config --global github.user "bh247484"
+# git config --global github.token "$ACC_KEY"
 
 git clone https://github.com/bh247484/actions-repo-2.git
 
@@ -43,7 +41,12 @@ git commit -m "Upversion to latest"
 # git remote add origin https://bh247484:"$ACC_KEY"@github.com/bh247484/actions-repo-2.git
 
 # git remote set-url origin https://bh247484:"$ACC_KEY"@github.com/bh247484/actions-repo-2.git
+git config --global credential.helper store
 git push origin -u pb-9.15.0-upgrade
+
+Username for 'https://github.com': "bh247484"
+Password for 'https://USERNAME@github.com': "$ACC_KEY"
+
 # expect "Username for 'https://github.com':"
 # send -- "bh247484"
 
