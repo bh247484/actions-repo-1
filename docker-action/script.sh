06e40059d96cc5020c0a582a01e18045faf017b7
Brendan Huffman <bh247484@gmail.com>
2021-05-21 08:47:03 -0400

    SSH attempt

--- docker-action/script.sh
+++ docker-action/script.sh
@@ -2,11 +2,16 @@
 
 echo ahoy from script.sh
 
-git config --global credential.helper store
+mkdir /root/.ssh/
 
-git config --global url.'https://bh247484:'"$ACC_KEY"':@github.com/'.insteadOf 'https://github.com/'
+echo "$ACC_KEY" > /root/.ssh/id_rsa
+echo “StrictHostKeyChecking no “ > /root/.ssh/config
 
-git config --global user.email "bh247484@gmail.com"
+# git config --global credential.helper store
+
+# git config --global url.'https://bh247484:'"$ACC_KEY"':@github.com/'.insteadOf 'https://github.com/'
+
+# git config --global user.email "bh247484@gmail.com"
 # git config --global user.name "$ACC_KEY"
 
 
@@ -15,7 +20,7 @@ git config --global user.email "bh247484@gmail.com"
 # Username for 'https://github.com/bh247484/actions-repo-2': bh247484
 # Password for 'https://USERNAME@some_git_server.com': <PASSWORD>
 
-git clone https://github.com/bh247484/actions-repo-2
+git clone git@github.com:bh247484/actions-repo-2.git
 cd actions-repo-2
 yarn upgrade playbook-ui@9.15.0
 cat package.json
@@ -24,6 +29,6 @@ git checkout -b pb-9.15.0-upgrade
 git add .
 git commit -m "Upversion to latest"
 
-git remote add origin https://bh247484:"$ACC_KEY"@github.com/bh247484/actions-repo-2.git
+# git remote add origin https://bh247484:"$ACC_KEY"@github.com/bh247484/actions-repo-2.git
 
 git push origin -u pb-9.15.0-upgrade
